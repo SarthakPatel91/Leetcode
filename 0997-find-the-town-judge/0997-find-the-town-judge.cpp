a7fc4c9dced5fcfe4a1a1047cc3b559b6e725021
SarthakPatel91 <patelsarthak194@gmail.com>
2026-07-05 14:25:51 +0530

    Time: 1 ms (58.81%), Space: 65 MB (83.27%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        if (n == 1)
-            return 1;
-            
-        // make graph
-        unordered_map<int, vector<int>> adj;
+
+        vector<int> indegree(n + 1, 0);
+        vector<int> outdegree(n + 1, 0);
+
+        // unordered_map<int, vector<int>> adj;
         for (int i = 0; i < trust.size(); i++) {
             int u = trust[i][0];
             int v = trust[i][1];
 
-            adj[u].push_back(v);
-        }
-        // indegree of each node
-        vector<int> indegree(n + 1, 0);
-        for (int u = 1; u <= n; u++) {
-            for (auto& v : adj[u])
-                indegree[v]++;
-        }
-        int judge = -1;
-        for (int i = 0; i < indegree.size(); i++) {
-            if (indegree[i] == n - 1) {
-                judge = i;
-                break;
-            }
-        }
-        for (int i = 0; i < trust.size(); i++) {
-            if (trust[i][0] == judge)
-                return -1;
+            indegree[v]++;
+            outdegree[u]++;
+
+            // adj[u].push_back(v);
         }
 
-        return judge;
+        for(int i=1;i<indegree.size();i++){
+            if(indegree[i]==n-1 && outdegree[i]==0)
+            return i;
+
+        }
+
+        return -1;
     }
 };
