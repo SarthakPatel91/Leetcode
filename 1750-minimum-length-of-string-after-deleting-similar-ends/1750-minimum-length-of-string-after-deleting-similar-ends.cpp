fe3e6e6a0574292dbedb5eaf11129a1a3f118f24
SarthakPatel91 <patelsarthak194@gmail.com>
2026-07-19 12:49:45 +0530

    Time: 0 ms (100%), Space: 15.9 MB (69.82%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -1,43 +1,19 @@
 class Solution {
 public:
     int minimumLength(string s) {
-
-        int n = s.size();
-        if (n == 1)
-            return 1;
-
-        if(s=="aba")
-        return 1;
-
         int i = 0;
-        int j = n - 1;
+        int j = s.size() - 1;
 
-        if (s[i] != s[j])
-            return s.size();
+        while (i < j && s[i] == s[j]) {
+            char ch = s[i];
 
-        string temp = s;
-        reverse(temp.begin(), temp.end());
-
-        if (s == temp)
-            return 0;
-
-        while (i < j) {
-            int ch = s[i];
-            i++;
-            j--;
-            while (ch == s[i]) {
+            while (i <= j && s[i] == ch)
                 i++;
-            }
-        
-            while (ch == s[j]) {
-                j--;
-            }
-            if (s[i] != s[j])
-                return j - i + 1;
-        }
-        if (i == j)
-            return 1;
 
-        return 0;
+            while (i <= j && s[j] == ch)
+                j--;
+        }
+
+        return j - i + 1;
     }
 };
