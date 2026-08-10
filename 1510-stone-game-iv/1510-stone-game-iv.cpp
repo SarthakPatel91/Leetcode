84850bf6cb5a401970ab02680c7eb80df629bc75
SarthakPatel91 <patelsarthak194@gmail.com>
2026-08-10 20:41:24 +0530

    Time: 42 ms (61.41%), Space: 18.2 MB (23.56%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool solve(int n, vector<int>& dp) {
+
+        if (n == 0)
+            return false;
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        for (int j = 1; j * j <= n; j++) {
+
+            // If this move gives opponent a losing state
+            if (!solve(n - j * j, dp))
+                return dp[n] = true;
+        }
+
+        return dp[n] = false;
+    }
+
+    bool winnerSquareGame(int n) {
+
+        vector<int> dp(n + 1, -1);
+
+        return solve(n, dp);
+    }
+};
