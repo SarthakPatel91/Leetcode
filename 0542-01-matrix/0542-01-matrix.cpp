a86a11d00cd43ac44dfeb7711a136a9417cc181a
SarthakPatel91 <patelsarthak194@gmail.com>
2026-08-23 00:14:52 +0530

    Time: 8 ms (91.26%), Space: 34.9 MB (61.82%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+
+        int m = mat.size();
+        int n = mat[0].size();
+
+        vector<vector<int>> res(m, vector<int>(n, -1));
+        queue<pair<int, int>> q;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    res[i][j] = 0;
+                    q.push({i, j});
+                }
+            }
+        }
+
+        while (!q.empty()) {
+            pair<int, int> p = q.front();
+            q.pop();
+
+            int i = p.first;
+            int j = p.second;
+            for (auto& dir : directions) {
+                int new_i = i + dir[0];
+                int new_j = j + dir[1];
+
+                if (new_i >= 0 && new_i < m && new_j >= 0 && new_j < n &&
+                    res[new_i][new_j] == -1) {
+                    res[new_i][new_j] = res[i][j] + 1;
+                    q.push({new_i, new_j});
+                }
+            }
+        }
+
+        return res;
+    }
+};
