1eddcc59e12ee5ceb3b1fe659b32e3c5bec7634f
SarthakPatel91 <patelsarthak194@gmail.com>
2026-08-26 13:04:33 +0530

    Time: 0 ms (100%), Space: 9.2 MB (32.75%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -3,59 +3,55 @@ public:
     string smallest(string& a, string& b) {
         if (a > b)
             return b;
-
         return a;
     }
+
     string shortestBeautifulSubstring(string s, int k) {
         vector<string> ans;
 
         int size = s.size();
-
         int len = INT_MAX;
-        string res = "";
         int count = 0;
-
         int i = 0;
 
         for (int j = 0; j < size; j++) {
-            // expand
-            res = res + s[j];
 
             if (s[j] == '1')
                 count++;
 
-            // shrink
             while (count > k) {
                 if (s[i] == '1')
                     count--;
-
                 i++;
             }
 
-            // update
             if (count == k) {
 
-                while (res[i] == '0')
+                while (s[i] == '0')
                     i++;
-                // for min length
-                if (len >= j - i + 1) {
-                    len = j - i + 1;
-                    ans.push_back(s.substr(i, j - i + 1));
+
+                int currLen = j - i + 1;
+
+                if (currLen < len) {
+                    len = currLen;
+                    ans.clear();
+                }
+
+                if (currLen == len) {
+                    ans.push_back(s.substr(i, currLen));
                 }
             }
         }
 
-        vector<string> temp;
-        for (int i = 0; i < ans.size(); i++) {
-            if (ans[i].size() == len)
-                temp.push_back(ans[i]);
-        }
-
-        if (count < k)
+        if (ans.empty())
             return "";
 
-        sort(temp.begin(), temp.end());
+        string answer = ans[0];
 
-        return temp[0];
+        for (int i = 1; i < ans.size(); i++) {
+            answer = smallest(answer, ans[i]);
+        }
+
+        return answer;
     }
 };
