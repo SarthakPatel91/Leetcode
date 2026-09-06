f4efadb894e4921b6451fdef994b8768d7ea2e2a
SarthakPatel91 <patelsarthak194@gmail.com>
2026-09-06 23:54:02 +0530

    Time: 0 ms (100%), Space: 9.1 MB (65.7%) - LeetHub

---
+++ 3813-vowel-consonant-score/3813-vowel-consonant-score.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int vowelConsonantScore(string s) {
+        int v = 0;
+        int c = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (isalpha(s[i])) {
+                if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' ||
+                    s[i] == 'u')
+                    v++;
+                else
+                    c++;
+            }
+        }
+
+        if (c == 0)
+            return 0;
+
+        int score = v / c;
+
+        return score;
+    }
+};
