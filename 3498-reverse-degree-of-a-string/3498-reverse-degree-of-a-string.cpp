fcd22093e8224b02d2668a4f2e0e349199dc917c
SarthakPatel91 <patelsarthak194@gmail.com>
2026-09-20 10:38:53 +0530

    Time: 183 ms (5.47%), Space: 10 MB (6.88%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        vector<int> freq(26, 0);
+        int size = s.size();
+
+        int ans=0;
+
+        for(int i=0;i<size;i++){
+            int idx='z'-s[i];
+            cout<<idx<<" ";
+            freq[idx]++;
+
+            ans+=(i+1)*(idx+1);
+        }
+
+        return ans;
+    }
+};
